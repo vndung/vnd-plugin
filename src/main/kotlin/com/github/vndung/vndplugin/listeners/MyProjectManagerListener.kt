aef6bd74e9a523a74e7cd34a4ebc45eaff5a957a
GitHub Action <action@github.com>
2020-10-13 08:56:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/vndung/vndplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/vndung/vndplugin/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.vndung.vndplugin.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.vndung.vndplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
